2a931a7115a842c32d9f68f543674ac797fecac8
Tiècoura DIARRA <tddiarra@odc.local>
2022-08-19 07:30:31 +0000

    Premiere version

---
+++ apitirage/src/main/java/com/example/apitirage/ApitirageApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitirage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitirageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitirageApplication.class, args);
+	}
+
+}
